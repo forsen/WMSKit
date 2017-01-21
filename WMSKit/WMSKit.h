c346ef3145bacf6593d9499ca7a053329b41aa5e
Erik Haider Forsén <erik@forsen.no>
2017-01-21 23:01:37 +0100

    Created XCode project

---
+++ WMSKit/WMSKit.h
@@ -0,0 +1,19 @@
+//
+//  WMSKit.h
+//  WMSKit
+//
+//  Created by Erik Haider Forsen on 21/01/2017.
+//  Copyright © 2017 Erik Haider Forsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WMSKit.
+FOUNDATION_EXPORT double WMSKitVersionNumber;
+
+//! Project version string for WMSKit.
+FOUNDATION_EXPORT const unsigned char WMSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WMSKit/PublicHeader.h>
+
+
